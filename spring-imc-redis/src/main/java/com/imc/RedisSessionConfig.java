1fa63b654cdf02fb7e0e5d6f681769ef19b35310
DESKTOP-HTDE4GJ\Rishabh Handa <rishabh.handa@inmindcloud.com>
2021-11-29 11:19:07 +0530

    cf service

--- spring-imc-redis/src/main/java/com/imc/RedisSessionConfig.java
+++ spring-imc-redis/src/main/java/com/imc/RedisSessionConfig.java
@@ -5,28 +5,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
+import java.time.Duration;
+
 @Configuration
 @EnableRedisHttpSession
-public class RedisSessionConfig extends AbstractHttpSessionApplicationInitializer{
+public class RedisSessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
     @Bean
     @Primary
     public RedisConnectionFactory redisConnectionFactory() {
 
-        RedisStandaloneConfiguration a = new RedisStandaloneConfiguration();
-        a.setPort(6379);
-        a.setHostName("localhost");
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+//        redisConfig.setPort(6379);
+//        redisConfig.setHostName("localhost");
 
-//        a.setPort(18601);
-//        a.setHostName("redis-18601.c250.eu-central-1-1.ec2.cloud.redislabs.com");
-//        a.setPassword("iRVy5Tbpg9z8txMpJ55WlutEsSWRSX4f");
+        // Enterprise cloud
+//        redisConfig.setPort(18601);
+//        redisConfig.setHostName("redis-18601.c250.eu-central-1-1.ec2.cloud.redislabs.com");
+//        redisConfig.setPassword("iRVy5Tbpg9z8txMpJ55WlutEsSWRSX4f");
 
-        return new JedisConnectionFactory(a);
+
+        // cf service
+        redisConfig.setPort(1249);
+        redisConfig.setHostName("master.rg-b53cf680-1fc8-41e2-8fa9-f3d045e784be.9u04ff.use1.cache.amazonaws.com");
+        redisConfig.setPassword("XhLjKapfymjoxhYBpQIimzPxDgjJLfyo");
+
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .readTimeout(Duration.ofSeconds(10))
+                .usePooling()
+                .and()
+                .useSsl()
+                .build();
+
+
+        return new JedisConnectionFactory(redisConfig, jedisClientConfiguration);
+
     }
 }
